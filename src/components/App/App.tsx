ddbbb67abe6304475d66c9db81c9eeac9958bdfb
OlgaKozoriz07 <161890591+OlgaKozoriz07@users.noreply.github.com>
2025-12-10 15:51:14 +0100

    update

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+function App() {
+    
+    return (
+        <>
+            <h1>Movie Search App</h1>
+        </>
+    );
+}
+
+export default App;
